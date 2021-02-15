28fa8699033f7faf92f9f04b392956f849ce1371
Greg Holmes <iam@gregholmes.co.uk>
2021-02-15 10:37:23 +0000

    replaced PHP sdk with Symfony specific wrapper

--- API/config/bundles.php
+++ API/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Vonage\ClientBundle\VonageClientBundle::class => ['all' => true],
 ];
